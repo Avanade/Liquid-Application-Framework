b097749ecb094dfeb5630771f9e3fc77409d4132
Luciana Regina Cardoso Lino <lucianareginalino@live.com>
2021-08-09 09:26:03 -0300

    feat: build simple sample api with a Get Action using liquid stable packages.

---
+++ Samples/src/Liquid.Domain/Handlers/SampleRequest.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+
+namespace Liquid.Sample.Domain.Handlers
+{
+    public class SampleRequest : IRequest<SampleResponse>
+    {
+        public int Id { get; set; }
+
+        public SampleRequest(int id)
+        {
+            Id = id;
+        }
+    }
+}

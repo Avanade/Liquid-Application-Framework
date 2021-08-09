5daf617458b64147dc99785e631f9f00d0167d3d
Luciana Regina Cardoso Lino <lucianareginalino@live.com>
2021-08-09 10:12:36 -0300

    feat: publish sample webapi.

---
+++ samples/src/Liquid.Sample.Domain/Handlers/SampleResponse.cs
@@ -0,0 +1,16 @@
+﻿using Liquid.Sample.Domain.Entities;
+using System.Collections.Generic;
+
+namespace Liquid.Sample.Domain.Handlers
+{
+    public class SampleResponse
+    {
+        public SampleEntity Response { get; set; }
+
+
+        public SampleResponse(SampleEntity response)
+        {
+            Response = response;
+        }
+    }
+}

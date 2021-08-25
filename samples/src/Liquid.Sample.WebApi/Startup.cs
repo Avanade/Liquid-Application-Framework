13142f6b09fbf25612ce1b7662e4def902abbff8
Luciana Regina Cardoso Lino <lucianareginalino@live.com>
2021-08-25 10:59:10 -0300

    fix: remove unnecessary dependency injection.

--- samples/src/Liquid.Sample.WebApi/Startup.cs
+++ samples/src/Liquid.Sample.WebApi/Startup.cs
@@ -16,7 +16,7 @@ namespace Liquid.Sample.WebApi
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddLiquidConfiguration();
+           
             services.AddLiquidMongoWithTelemetry<SampleEntity, int>();
 
             services.AddLiquidHttp(typeof(SampleRequest).Assembly);

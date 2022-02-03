3cd01996a161f4a55c2c9efc8dc657c8805ca331
Luciana Regina Cardoso Lino <luciana.regina.lino@avanade.com>
2022-02-03 11:33:40 -0300

    feat(Liquid.Templates.csproj): create liquid template project with Liquid.Crud.Solution template.

---
+++ templates/src/Liquid.Templates/Templates/Liquid.Crud.Solution/src/PROJECTNAME.Domain/Handlers/ENTITYNAME/Read/GetByIdENTITYNAMEQueryHandler.cs
@@ -0,0 +1,26 @@
+﻿using Liquid.Repository;
+using MediatR;
+using PROJECTNAME.Domain.Entities;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace PROJECTNAME.Domain.Handlers.ENTITYNAME.Read
+{
+    public class GetByIdENTITYNAMEQueryHandler : IRequestHandler<GetByIdENTITYNAMEQuery, GetByIdENTITYNAMEQueryResponse>
+    {
+        private readonly ILiquidRepository<ENTITYNAMEEntity, int> _repository;
+
+        public GetByIdENTITYNAMEQueryHandler(ILiquidRepository<ENTITYNAMEEntity, int> repository)
+        {
+            _repository = repository;
+        }
+
+        ///<inheritdoc/>        
+        public async Task<GetByIdENTITYNAMEQueryResponse> Handle(GetByIdENTITYNAMEQuery request, CancellationToken cancellationToken)
+        {
+            var data = await _repository.FindByIdAsync(request.Id);
+
+            return new GetByIdENTITYNAMEQueryResponse(data);
+        }
+    }
+}

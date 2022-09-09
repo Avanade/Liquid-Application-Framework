37cbb558629f6fa500404ff0655313f1ba594c8c
Luciana Regina Cardoso Lino <luciana.regina.lino@avanade.com>
2022-09-09 15:58:07 -0300

    feat(Liquid.Cache.SqlServer): Build Liquid.Cache.SqlServer package. This implementation just extends  Microsoft.Extensions.Caching.SqlServer DI by including Liquid.Cache decorator registry. Resolves #144 Signed-off-by: Luciana Regina Lino <luciana.regina.lino@avanade.com>

---
+++ src/Liquid.Cache.SqlServer/Extensions/DependencyInjection/IServiceCollectionExtension.cs
@@ -0,0 +1,30 @@
+﻿using Liquid.Cache.Extensions.DependencyInjection;
+using Liquid.Core.Implementations;
+using Microsoft.Extensions.Caching.SqlServer;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace Liquid.Cache.SqlServer.Extensions.DependencyInjection
+{
+    /// <summary>
+    /// LiquidCache using SqlServer <see cref="IServiceCollection"/> extensions class.
+    /// </summary>
+    public static class IServiceCollectionExtension
+    {
+        /// <summary>
+        /// Registers <see cref="SqlServerCache"/> service and <see cref="LiquidCache"/> decorator,
+        /// with its <see cref="LiquidTelemetryInterceptor"/>.
+        /// </summary>
+        /// <param name="services"></param>
+        /// <param name="setupAction">An System.Action`1 to configure the provided
+        ///<see cref="SqlServerCacheOptions"/>.</param>
+        /// <param name="withTelemetry">Indicates if this method must register a <see cref="LiquidTelemetryInterceptor"/></param>
+        public static IServiceCollection AddLiquidSqlServerDistributedCache(this IServiceCollection services,
+            Action<SqlServerCacheOptions> setupAction, bool withTelemetry = true)
+        {
+            services.AddDistributedSqlServerCache(setupAction);
+
+            return services.AddLiquidDistributedCache(withTelemetry);
+        }
+    }
+}

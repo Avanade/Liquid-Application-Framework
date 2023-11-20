94ca64763f7c6dcab96c2033f570ea7cd2e1a22a
Luciana Regina Lino <luciana.regina.lino@avanade.com>
2023-11-20 16:30:48 -0300

    fix(DataMappingException): Includes "ExcludeFromCodeCoverage" attribute.

--- src/Liquid.Adapter.Dataverse/DataMappingException.cs
+++ src/Liquid.Adapter.Dataverse/DataMappingException.cs
@@ -1,8 +1,10 @@
-﻿using System.Runtime.Serialization;
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Runtime.Serialization;
 
 namespace Liquid.Adapter.Dataverse
 {
     [Serializable]
+    [ExcludeFromCodeCoverage]
     internal class DataMappingException : Exception
     {
         public DataMappingException()

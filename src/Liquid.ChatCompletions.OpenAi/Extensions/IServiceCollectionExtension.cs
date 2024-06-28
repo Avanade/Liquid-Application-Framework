bc9c5c0aa4f5cbf7da5a19756fc622be4a7d1875
Luciana Regina Lino <luciana.regina.lino@avanade.com>
2024-06-28 14:22:21 -0300

    fix: namespaces.

--- src/Liquid.ChatCompletions.OpenAi/Extensions/IServiceCollectionExtension.cs
+++ src/Liquid.ChatCompletions.OpenAi/Extensions/IServiceCollectionExtension.cs
@@ -2,7 +2,6 @@
 using Liquid.Core.Settings;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Pixel.Platform.Infra.OpenAi;
 using System.Diagnostics.CodeAnalysis;
 
 namespace Liquid.ChatCompletions.OpenAi.Extensions
